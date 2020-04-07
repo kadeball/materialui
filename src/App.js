ca5617a8d1590553430e1ee4fe38e6022fddd6bd
kadeball <kadeball@iu.edu>
2020-04-06 20:22:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import TypoGraphy from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import Nav from "./components/Nav";
+import Checkboxed from "./components/Checkboxed";
+import Snackbars from "./components/Snackbars";
+import Steppers from "./components/Steppers";
+import Text from "./components/Text";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <AppBar color="primary" position="static">
+          <Nav>
+            <Toolbar>
+              <TypoGraphy variant="title"
+                          color="inherit"
+              >
+                Cool Stuff
+              </TypoGraphy>
+            </Toolbar>
+          </Nav>
+        </AppBar>
+
+        <Checkboxed/>
+        <Snackbars/>
+        <Steppers/>
+        <Text/>
+
+
+      </div>
   );
 }
+
 
 export default App;
